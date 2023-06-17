130f4f0a07ba3453a1cf53f2ea555db0b59d9a19
Ephraim Park <ephiepark@gmail.com>
2023-06-17 12:18:52 -0700

    Firebase Integration

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcUWQMN0qD_lQqW8jeg6tjh0a9cXynVzM",
+  authDomain: "rn-expo-template.firebaseapp.com",
+  projectId: "rn-expo-template",
+  storageBucket: "rn-expo-template.appspot.com",
+  messagingSenderId: "724335117582",
+  appId: "1:724335117582:web:cba4444f14324a45a0435f",
+  measurementId: "G-9QDFVCYZT0"
+};
